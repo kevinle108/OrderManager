b05ac00cc76c07958c1474753a20940dc69916ee
Kevin Le <kevinle108@gmail.com>
2020-10-21 13:06:31 -0400

    added properties and constuctor to the Order class

--- OrderManager/Order.cs
+++ OrderManager/Order.cs
@@ -6,5 +6,19 @@ namespace OrderManager
 {
     class Order
     {
+        public string Item { get; set; }
+        public string Store { get; set; }
+        public double Price { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+
+        public Order(string item, string store, double price, string orderDate, string arrivalDate)
+        {
+            Item = item.ToUpper();
+            Store = store.ToUpper();
+            Price = price;
+            OrderDate = DateTime.Parse(orderDate);
+            ArrivalDate = DateTime.Parse(arrivalDate);
+        }
     }
 }

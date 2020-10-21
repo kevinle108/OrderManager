42107fd522e0c8abc1dc5ee7c3575b341896b160
Kevin Le <kevinle108@gmail.com>
2020-10-21 13:43:03 -0400

    add a global CreatOrder method

--- OrderManager/Program.cs
+++ OrderManager/Program.cs
@@ -4,7 +4,11 @@ namespace OrderManager
 {
     class Program
     {
-
+        public static Order CreateOrder(string str)
+        {
+            string[] arr = str.Split(',');
+            return new Order(arr[0], arr[1], Double.Parse(arr[2]), arr[3], arr[4]);
+        }
 
         static void Main(string[] args)
         {
